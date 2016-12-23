a4e53c309457bf44af4f371af0d96f32b04a33a6
superping <superping2006@163.com>
2016-12-24 00:36:24 +0800

    Update getMoney.py
    use arg to set input file and start pixel of Y axis

--- src/getMoney.py
+++ src/getMoney.py
@@ -1,35 +1,61 @@
 #!/usr/bin/env python
 # created by yao
-# improved by superping
+# inproved by superping
 # 2016.12.23
-import sys
+import sys, getopt
 from PIL import Image
-im = Image.open(sys.argv[1])
-
-xsize, ysize = im.size
-#imgWidth = 370 # width of the image you cut off
-startY = 18 # the first line position-y
-splitPoxis = 18 # split height
-blackHeigh = 8 # black line height
-maxLineNumber = 27
 
 
-# function to deal image
-def pasteImg( startY, index):
-    box = (1, startY + splitPoxis*index-blackHeigh,xsize,startY + splitPoxis*index)
-    region = im.crop(box)
-    
-    box_dealed = (1,startY + splitPoxis * index,xsize,startY + splitPoxis*index+blackHeigh)
-    region_dealed = im.crop(box_dealed)
-    
-    im.paste( region, box_dealed )
+def main(argv):
+	inputfile = ''
+	start = 12
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hi:s:",["ifile=","startY="])
+	except getopt.GetoptError:
+		print 'getMoney.py -i <inputfile> -s <startY>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'getMoney.py -i <inputfile> -s <startY>'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-s", "--start"):
+			start = arg
+	print 'inputfile:', inputfile
+	print 'startY:', start
 
 
-pasteImg(startY, 0)
+	im = Image.open(inputfile)
 
-for index in range(1,maxLineNumber):
-    pasteImg( startY, index )
+	xsize, ysize = im.size
+	#imgWidth = 370 # width of the image you cut off
+	startY = 18 # the first line position-y
+	splitPoxis = 18 # split height
+	blackHeigh = 8 # black line height
+	maxLineNumber = 27
 
 
-im.save(sys.argv[1]+'.dealed.jpg')
-im.show()
+	# function to deal image
+	def pasteImg( startY, index):
+	    box = (1, startY + splitPoxis*index-blackHeigh,xsize,startY + splitPoxis*index)
+	    region = im.crop(box)
+	    
+	    box_dealed = (1,startY + splitPoxis * index,xsize,startY + splitPoxis*index+blackHeigh)
+	    region_dealed = im.crop(box_dealed)
+	    
+	    im.paste( region, box_dealed )
+
+
+	pasteImg(startY, 0)
+
+	for index in range(1,maxLineNumber):
+		pasteImg( startY, index )
+
+
+	im.save(inputfile+'.dealed.jpg')
+	im.show()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
