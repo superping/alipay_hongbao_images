7b0f5fe42a238895d14dcba5f70eb4bbcb49ef62
superping <superping2006@163.com>
2016-12-23 22:27:11 +0800

    Update getMoney.py
    use argv

--- src/getMoney.py
+++ src/getMoney.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # created by yao
+# improve by superping
 # 2016.12.23
+import sys
 from PIL import Image
-im = Image.open('1239788822.jpg')
+im = Image.open(sys.argv[1])
 
-imgWidth = 370 # width of the image you cut off
-startY = 10 # the first line position-y
-splitPoxis = 13 # split height
-blackHeigh = 6 # black line height
+xsize, ysize = im.size
+#imgWidth = 370 # width of the image you cut off
+startY = 18 # the first line position-y
+splitPoxis = 18 # split height
+blackHeigh = 8 # black line height
 maxLineNumber = 27
 
 
 # function to deal image
 def pasteImg( startY, index):
-    box = (1, startY + splitPoxis*index-blackHeigh,imgWidth,startY + splitPoxis*index)
+    box = (1, startY + splitPoxis*index-blackHeigh,xsize,startY + splitPoxis*index)
     region = im.crop(box)
     
-    box_dealed = (1,startY + splitPoxis * index,imgWidth,startY + splitPoxis*index+blackHeigh)
+    box_dealed = (1,startY + splitPoxis * index,xsize,startY + splitPoxis*index+blackHeigh)
     region_dealed = im.crop(box_dealed)
     
     im.paste( region, box_dealed )
